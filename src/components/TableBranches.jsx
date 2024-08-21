34863b7c14df41c33e361af6805f8ed0ccf2c35a
ArrushC <contact@arrushc.com>
2024-08-21 15:53:35 +0100

    Make branches table responsive to window height

--- src/components/TableBranches.jsx
+++ src/components/TableBranches.jsx
@@ -1,12 +1,15 @@
 import { CopyIcon, DragHandleIcon } from "@chakra-ui/icons";
 import { AgGridReact } from "ag-grid-react";
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useApp } from "../AppContext";
 import { IconButton, Tooltip } from "@chakra-ui/react";
 import { stripBranchInfo } from "../utils/CommonConfig";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 export default function TableBranches({ rowData, onRowValueChanged }) {
 	const { config, branchTableGridRef, updateConfig, isDebug, selectedBranches, setSelectedBranches, setSelectedBranchStatuses, setShowCommitView } = useApp();
+	const windowDimensions = useWindowDimensions();
+	const [isTallScreen, setIsTallScreen] = useState(windowDimensions.height > 768);
 
 	const copyRow = useCallback(
 		(currentRowData) => {
@@ -46,15 +49,15 @@ export default function TableBranches({ rowData, onRowValueChanged }) {
 		[isDebug, branchTableGridRef, selectedBranches, setSelectedBranches, setShowCommitView]
 	);
 
-	// const onRowDragEnd = useCallback(
-	// 	(event) => {
-	// 		const movedRowData = [];
-	// 		event.api.forEachNode((node) => movedRowData.push(node.data));
-	// 		if (isDebug) console.debug("SectionBranches.jsx: onRowDragEnd - movedRowData", movedRowData);
-	// 		updateConfig((currentConfig) => ({ ...currentConfig, branches: stripBranchInfo(movedRowData) }));
-	// 	},
-	// 	[updateConfig, isDebug]
-	// );
+	const onRowDragEnd = useCallback(
+		(event) => {
+			const movedRowData = [];
+			event.api.forEachNode((node) => movedRowData.push(node.data));
+			if (isDebug) console.debug("SectionBranches.jsx: onRowDragEnd - movedRowData", movedRowData);
+			updateConfig((currentConfig) => ({ ...currentConfig, branches: stripBranchInfo(movedRowData) }));
+		},
+		[updateConfig, isDebug]
+	);
 
 	const getRowStyle = useCallback(
 		(params) => {
@@ -80,9 +83,8 @@ export default function TableBranches({ rowData, onRowValueChanged }) {
 		[]
 	);
 
-	const colDefs = useMemo(
-		() => [
-			// { field: "", rowDrag: true, resizable: false, filter: false, suppressMovable: false, editable: false, width: 20, cellRenderer: DragHandleIcon, headerClass: "branch-table-header-cell", cellClass: "branch-table-body-cell" },
+	const colDefs = useMemo(() => {
+		const commonColDefs = [
 			{ headerCheckboxSelection: true, checkboxSelection: true, headerCheckboxSelectionFilteredOnly: true, width: 20, resizable: false, suppressMovable: false, filter: false, editable: false, headerClass: "branch-table-header-cell", cellClass: "branch-table-body-cell" },
 			{
 				headerName: "",
@@ -101,12 +103,17 @@ export default function TableBranches({ rowData, onRowValueChanged }) {
 			{ field: "Branch Version", resizable: false, width: 130 },
 			{ field: "SVN Branch", flex: 1 },
 			{ field: "Branch Info", editable: false, resizable: false, width: 200 },
-		],
-		[config, copyRow]
-	);
+		];
+
+		return isTallScreen ? [{ field: "", rowDrag: true, resizable: false, filter: false, suppressMovable: false, editable: false, width: 20, cellRenderer: DragHandleIcon, headerClass: "branch-table-header-cell", cellClass: "branch-table-body-cell" }, ...commonColDefs] : commonColDefs;
+	}, [config, isTallScreen, copyRow]);
+
+	useEffect(() => {
+		setIsTallScreen(windowDimensions.height > 768);
+	}, [windowDimensions]);
 
 	return (
-		<div className="ag-theme-balham-dark compact" style={{ height: "428px", width: "100%"}}>
+		<div className="ag-theme-balham-dark compact" style={{ height: isTallScreen ? "auto" : "428px", width: "100%" }}>
 			<AgGridReact
 				ref={branchTableGridRef}
 				rowData={rowData}
@@ -114,8 +121,8 @@ export default function TableBranches({ rowData, onRowValueChanged }) {
 				columnDefs={colDefs}
 				stopEditingWhenCellsLoseFocus={true}
 				getRowStyle={getRowStyle}
-				// onRowDragEnd={onRowDragEnd}
-				domLayout="normal"
+				onRowDragEnd={onRowDragEnd}
+				domLayout={isTallScreen ? "autoHeight" : "normal"}
 				suppressRowClickSelection={true}
 				rowSelection={"multiple"}
 				rowMultiSelectWithClick={true}
@@ -131,7 +138,7 @@ export default function TableBranches({ rowData, onRowValueChanged }) {
 				suppressFlash={true}
 				enableCellTextSelection={true}
 				ensureDomOrder
-				pagination={true}
+				pagination={!isTallScreen}
 				paginationAutoPageSize={false}
 				paginationPageSizeSelector={false}
 				paginationPageSize={10}

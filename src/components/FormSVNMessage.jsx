4522e6c0d84abe84d0aca03a93ec6523cdd84d7d
ArrushC <contact@arrushc.com>
2024-08-27 20:59:50 +0100

    Add other issue numbers

--- src/components/FormSVNMessage.jsx
+++ src/components/FormSVNMessage.jsx
@@ -1,4 +1,4 @@
-import { Box, Flex, FormControl, FormLabel, IconButton, Textarea, Tooltip } from "@chakra-ui/react";
+import { Box, Flex, FormControl, FormLabel, IconButton, Textarea, Tooltip, Text } from "@chakra-ui/react";
 import { Select } from "chakra-react-select";
 import React, { useCallback, useEffect } from "react";
 import { useApp } from "../AppContext";
@@ -8,9 +8,9 @@ import useCommitOptions from "../hooks/useCommitOptions";
 import useConfigUtilities from "../hooks/useConfigUtilities";
 
 export default function FormSVNMessage({ openMessageAutoFillModal }) {
-	const { sourceBranch, setSourceBranch, branchOptions, setIssueNumber, commitMessage, setCommitMessage, isCommitMode } = useApp();
+	const { sourceBranch, setSourceBranch, branchOptions, setIssueNumber, commitMessage, setCommitMessage, isCommitMode, selectedBranches } = useApp();
 	const commitOptions = useCommitOptions();
-	const {getBranchFolderById} = useConfigUtilities();
+	const { getBranchFolderById } = useConfigUtilities();
 
 	const handleSourceBranchChange = useCallback(
 		(selectedOption) => {
@@ -46,7 +46,7 @@ export default function FormSVNMessage({ openMessageAutoFillModal }) {
 						</Box>
 						<Flex width={"50%"} alignItems={"flex-end"} columnGap={2}>
 							<FormControl isRequired>
-								<FormLabel>{sourceBranch && sourceBranch.value ? `Issue Number for ${getBranchFolderById(sourceBranch.value)}` : "Issue Number"}</FormLabel>
+								<FormLabel>{sourceBranch && sourceBranch.value ? `Issue Number For ${getBranchFolderById(sourceBranch.value)}` : "Issue Number"}</FormLabel>
 								<IssueNumberInput />
 							</FormControl>
 						</Flex>
@@ -57,6 +57,22 @@ export default function FormSVNMessage({ openMessageAutoFillModal }) {
 							<Textarea placeholder={"Enter Commit Message"} resize={"vertical"} onInput={handleCommitMessageChange} value={commitMessage} />
 						</FormControl>
 
+						{commitOptions?.useIssuePerFolder && sourceBranch && sourceBranch.value ? (
+							<Box width={"50%"}>
+								{selectedBranches.length > 1 ? selectedBranches.filter((branch) => branch.id != sourceBranch.value).map((branch) => (
+									<FormControl key={branch.id} isRequired>
+										<FormLabel>{`Issue Number For ${getBranchFolderById(branch.id)}`}</FormLabel>
+										<IssueNumberInput />
+									</FormControl>
+								)) : null}
+							</Box>
+						) : !sourceBranch || !sourceBranch.value ? (
+							<Box width={"50%"} textAlign={"center"} m={"auto"}>
+								<Text fontSize={"lg"} fontWeight={"600"} color={"yellow.500"}>
+									Please select source branch first!
+								</Text>
+							</Box>
+						) : null}
 					</Flex>
 				</Box>
 				<Box pt={8}>

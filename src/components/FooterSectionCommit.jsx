029633a678de7f61d4d32f08e998af46c2259add
ArrushC <contact@arrushc.com>
2024-08-30 22:00:20 +0100

    Validate issue numbers based on data structural changes

--- src/components/FooterSectionCommit.jsx
+++ src/components/FooterSectionCommit.jsx
@@ -4,10 +4,13 @@ import React, { useCallback } from "react";
 import { MdCloudUpload } from "react-icons/md";
 import { useApp } from "../AppContext";
 import useNotifications from "../hooks/useNotifications";
+import _ from "lodash";
+import useConfigUtilities from "../hooks/useConfigUtilities";
 
 export default function FooterSectionCommit({ openCommitModal }) {
 	const { setShowCommitView, selectedLocalChanges, sourceBranch, issueNumber, commitMessage, setSocketPayload, configurableRowData } = useApp();
 	const { RaiseClientNotificaiton } = useNotifications();
+	const { getIssueNumbers } = useConfigUtilities();
 
 	const performRefresh = useCallback(() => {
 		setShowCommitView(false);
@@ -21,7 +24,7 @@ export default function FooterSectionCommit({ openCommitModal }) {
 		}
 
 		// Check if the issue number and commit message is provided
-		if (!issueNumber || !commitMessage  || issueNumber === "" || commitMessage === "") {
+		if (!issueNumber || _.isEmpty(issueNumber) || getIssueNumbers(true).includes("") || !commitMessage || commitMessage === "") {
 			RaiseClientNotificaiton("Please provide the issue number and the commit message to proceed!", "error");
 			return;
 		}
@@ -44,7 +47,8 @@ export default function FooterSectionCommit({ openCommitModal }) {
 				</Button>
 				<Tooltip label={"Select at least 1 file"} hasArrow isDisabled={selectedLocalChanges.length > 0}>
 					<Button onClick={performCommit} leftIcon={<Icon as={MdCloudUpload} />} colorScheme={"yellow"} isDisabled={selectedLocalChanges.length < 1}>
-						Commit {selectedLocalChanges.length > 0 ? `${selectedLocalChanges.length} File` : ""}{selectedLocalChanges.length > 1 ? "s" : ""}
+						Commit {selectedLocalChanges.length > 0 ? `${selectedLocalChanges.length} File` : ""}
+						{selectedLocalChanges.length > 1 ? "s" : ""}
 					</Button>
 				</Tooltip>
 			</Flex>

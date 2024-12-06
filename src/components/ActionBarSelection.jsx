8f6d213101c15effc0d6261dd4d4e6ea6745bb92
ArrushC <contact@arrushc.com>
2024-12-06 10:51:51 +0000

    Use brighter colours for the action bar to make it more visible to the user

--- src/components/ActionBarSelection.jsx
+++ src/components/ActionBarSelection.jsx
@@ -8,24 +8,24 @@ const ActionBarSelection = memo(({ selectedCount, onDelete, onRefresh, onUpdate,
 
 	return (
 		<ActionBarRoot open={true} closeOnEscape={false}>
-			<ActionBarContent>
-				<ActionBarSelectionTrigger>{selectedCount} Selected</ActionBarSelectionTrigger>
+			<ActionBarContent colorPalette={"yellow"}>
+				<ActionBarSelectionTrigger >{selectedCount} Selected</ActionBarSelectionTrigger>
 				<ActionBarSeparator />
 				<HStack wrap="wrap">
-					<Button variant="outline" size="sm" onClick={onDelete}>
-						Delete <Kbd wordSpacing={0}>Del</Kbd>
+					<Button variant="subtle" size="sm" onClick={onDelete}>
+						Delete <Kbd variant={"subtle"} wordSpacing={0}>Del</Kbd>
 					</Button>
-					<Button variant="outline" size="sm" onClick={onRefresh}>
-						Refresh <Kbd wordSpacing={0}>R</Kbd>
+					<Button variant="subtle" size="sm" onClick={onRefresh}>
+						Refresh <Kbd variant={"subtle"} wordSpacing={0}>R</Kbd>
 					</Button>
-					<Button variant="outline" size="sm" onClick={onUpdate}>
-						Update <Kbd wordSpacing={0}>U</Kbd>
+					<Button variant="subtle" size="sm" onClick={onUpdate}>
+						Update <Kbd variant={"subtle"} wordSpacing={0}>U</Kbd>
 					</Button>
-					<Button variant="outline" size="sm" onClick={onCommit}>
-						Commit <Kbd wordSpacing={0}>C</Kbd>
+					<Button variant="subtle" size="sm" onClick={onCommit}>
+						Commit <Kbd variant={"subtle"} wordSpacing={0}>C</Kbd>
 					</Button>
-					<Button variant="outline" size="sm" onClick={onLogs}>
-						Logs <Kbd wordSpacing={0}>L</Kbd>
+					<Button variant="subtle" size="sm" onClick={onLogs}>
+						Logs <Kbd variant={"subtle"} wordSpacing={0}>L</Kbd>
 					</Button>
 				</HStack>
 				<ActionBarSeparator />
